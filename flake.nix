724836d267e2449c76f7bc0ced896f1cfbdacdd1
apartapatia <raupov.deal@yandex.ru>
2026-05-31 23:40:11 +0300

    add first nix impl

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development environment for ITMO projet";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            vault
+          ];
+        };
+      }
+    );
+}
